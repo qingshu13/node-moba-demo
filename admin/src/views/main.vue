bfaf50bec6897baf8bf94fe36adfa1230726453e
qingshu13 <bj_zjd@126.com>
2021-05-20 14:04:20 +0800

    方式

--- admin/src/views/main.vue
+++ admin/src/views/main.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-container style="height: 500px; border: 1px solid #eee">
-      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+      <el-aside width="200px">
         <el-menu router :default-openeds="['1', '3']">
           <el-submenu index="1">
             <template slot="title"><i class="el-icon-message"></i>分类管理</template>
--- serve/index.js
+++ serve/index.js
@@ -1,3 +1,26 @@
 const express = require('express')
 
 const app = express()
+
+app.get('/demo',(req,res) =>{
+  // res.json({
+  //   message:'1111'
+  // })
+  throw new Error('测试异常功能')
+})
+
+function error_handle_middleaware(err,req,res,next){
+  let {message} = err
+  if(err){
+    res.status(500).json({
+      message:`${message || '服务器异常'}`
+    })
+  }else{
+    //
+  }
+}
+app.use(error_handle_middleaware)
+
+app.listen('5000',() =>{
+  console.log('启动成功')
+})
